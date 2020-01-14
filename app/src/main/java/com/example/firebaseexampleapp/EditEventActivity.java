966bbb08f4fb74a06e52d2f22410e6bd9baffc9d
mr-yman <gabriel.classon@gmail.com>
2020-01-14 12:00:30 -0600

    made it so that the calendar will show the currently selected date instead of today's date when it pops up.

--- app/src/main/java/com/example/firebaseexampleapp/EditEventActivity.java
+++ app/src/main/java/com/example/firebaseexampleapp/EditEventActivity.java
@@ -112,12 +112,9 @@ public class EditEventActivity extends AppCompatActivity {
         public Dialog onCreateDialog(Bundle savedInstanceState) {
             // Use the current date as the default date in the picker
             final Calendar c = Calendar.getInstance();
-            int year = c.get(Calendar.YEAR);
-            int month = c.get(Calendar.MONTH);
-            int day = c.get(Calendar.DAY_OF_MONTH);
 
             // Create a new instance of DatePickerDialog and return it
-            return new DatePickerDialog(getActivity(), this, year, month, day);
+            return new DatePickerDialog(getActivity(), this, currentActivity.getYear(), currentActivity.getMonth(), currentActivity.getDay());
         }
 
         public void onDateSet(DatePicker view, int year, int month, int date){
